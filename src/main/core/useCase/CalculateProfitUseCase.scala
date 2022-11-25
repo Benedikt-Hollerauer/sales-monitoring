31418bd219eb6837a9c0d68cfd5b185f914b5f0d
Benedikt Hollerauer <hollibene@gmail.com>
2022-11-25 10:40:26 +0100

    small cleanup

--- src/main/core/useCase/CalculateProfitUseCase.scala
+++ src/main/core/useCase/CalculateProfitUseCase.scala
@@ -8,41 +8,7 @@ import zio.*
 case class CalculateProfitUseCase private(
     input: CalculateProfitInput
 ):
-    def calculateProfit: IO[SaleEntityError, MoneyValue] =
-        val mayBeProfit = for
-            saleEntity <- input.sales
-            profit <- saleEntity.calculateProfit
-        yield NonEmptyChunk(profit)/*.reduce(_.amount + _.amount)*/
-
-        input.sales.map(saleEntity =>
-            NonEmptyChunk(
-                for
-                    profit <- saleEntity.calculateProfit.catchAll(error => ZIO.fail(error))
-                yield profit
-            )
-        )
-        for
-            calculateProfit <- input.sales
-            profit = calculateProfit.calculateProfit
-
-        yield ???
-
-        val test = input.sales
-            .flatMap(saleEntity => NonEmptyChunk(saleEntity.calculateProfit))
-
-        val test2 = test.map(x => (x.map(y => NonEmptyChunk(y))).map(moin => moin))
-
-        val test3 = test2.map(x => x.flatMap(y => y.reduce((x1, x2) => ZIO.succeed(x1.amount + x2.amount))))
-
-
-
-        input.sales.reduce((x, y) =>
-            for
-                first <- x.calculateProfit.catchAll(error => ZIO.fail(error))
-                second <- y.calculateProfit.catchAll(error => ZIO.fail(error))
-                moinsen <- MoneyValue.fromDouble(first.amount + second.amount).catchAll(error => ZIO.fail(SaleEntityError.ProfitConstructionFailed(error)))
-            yield moinsen
-        )
+    def calculateProfit: IO[SaleEntityError, MoneyValue] = ???
 
 object CalculateProfitUseCase:
 
